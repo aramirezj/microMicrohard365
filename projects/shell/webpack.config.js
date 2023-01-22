b7dc9d4d71af29a8135ba9594a081007deeeaac4
Alejandro <alejandroramirezjimenez1999@gmail.com>
2023-01-22 19:11:43 +0100

    Adjusted dependencys

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,4 +1,4 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { withModuleFederationPlugin, share } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -6,8 +6,33 @@ module.exports = withModuleFederationPlugin({
     "worldpreso": "http://localhost:4201/remoteEntry.js",
   },
 
-  shared: {
+  //If we want to every project has the exact dependency
+  /**
+   * shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
-
+   */
+  //To be able to specify certain dependencys
+  shared:
+    share({
+      "@angular/core": {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      },
+      "@angular/common": {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      },
+      "@angular/common/http": {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      },
+      "@angular/router": {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto'
+      },
+    })
 });
